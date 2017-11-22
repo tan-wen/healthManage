7b8ce612afd196148c738fc320b4a59d0c5d0f8f
wentan <872701869@qq.com>
2017-11-22 09:39:38 +0800

    添加Redis缓存功能

--- src/main/java/com/aoyang/health/admin/service/impl/UserServiceImpl.java
+++ src/main/java/com/aoyang/health/admin/service/impl/UserServiceImpl.java
@@ -1,19 +1,29 @@
 package com.aoyang.health.admin.service.impl;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
+import com.alibaba.fastjson.JSON;
 import com.aoyang.health.admin.dao.UserDao;
 import com.aoyang.health.admin.entity.User;
 import com.aoyang.health.admin.service.UserService;
+import com.google.gson.Gson;
 
 @Service
 public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserDao dao;
+	
+	@Autowired
+	private RedisTemplate<String, User> redisTemplate;
 	
 	@Override
 	public void save(User e) {
@@ -35,7 +45,23 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public List<User> findAll() {
-		return dao.findAll();
+		List<User> users = new ArrayList<>();
+		Set<String> sets = redisTemplate.keys("USER*");
+		if (sets == null || sets.isEmpty()) {
+			users = dao.findAll();
+			if (!CollectionUtils.isEmpty(users)) {
+				for (User user : users) {
+					redisTemplate.opsForValue().set("USER"+user.getId(), user);
+					System.out.println("redis存入用户信息：" + new Gson().toJson(user));
+				}
+			}
+		} else {
+			for (String userId : sets) {
+				users.add(redisTemplate.opsForValue().get(userId));
+				System.out.println("redis读取用户信息:" + userId);
+			}
+		}
+		return users;
 	}
 
 }
